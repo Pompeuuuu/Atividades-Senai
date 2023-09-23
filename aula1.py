c4ecb9778df8158869cc2e6c1574c49f0cf32991
Pompeuuuu <141966855+Pompeuuuu@users.noreply.github.com>
2023-09-23 17:17:00 -0300

    Add files via upload

---
+++ aula1.py
@@ -0,0 +1,10 @@
+name = str(input("Digite seu nome:"))
+number1 = int10(input("Digite o numero 1:"))
+number2 = int(input("Digite o numero 2:"))
+bemvindo=',prazer em te conhecer!'
+soma = number1+number2
+mult = number1*number2
+
+print(name, bemvindo)
+print('A soma de',number1, 'e', number2, 'é:',soma)
+print('A multiplicação de',number1, 'e', number2, 'é:',mult)
