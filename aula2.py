845db0fab5c2627cb18a0dde18f5862fe7560c78
Pompeuuuu <141966855+Pompeuuuu@users.noreply.github.com>
2023-09-30 14:14:17 -0300

    Add files via upload

---
+++ aula2.py
@@ -0,0 +1,39 @@
+# number = int(input("Digite o numero:"))
+# quadrado=number*number
+# print(f'O quadrado {number} é {quadrado}')
+
+# name = input("Digite seu primeiro nome:")
+# sobrenome = input("Digite seu segundo nome:")
+# print('Olá, ',name, sobrenome)
+
+# n1 = input("Digite o numero 1:")
+# n2 = input("Digite o numero 2:")
+# print(n1+n2)
+
+# python ="python"
+# numero=int(input("Digite um numero:"))
+# print(python, numero)
+
+
+# verb=input("Digite um verbo:")
+# print(f'De noite eu gosto de {verb}')
+
+# seg=input("Digite o segundo: ")
+# minu=input("Digite o minuto: ")
+# hour=input("Digite a hora: ")
+# print(f'{hour}h:{minu}m:{seg}s')
+
+# ddd=input("Digite o seu DDD: ")
+# tel=input("Digite o seu numero: ")
+# print(f'{ddd}-{tel}')
+
+# item1 = input("Primeiro item da lista:")
+# item2 = input("Segundo item da lista:")
+# item3 = input("Terceiro item da lista:")
+# item4 = input("Quarto item da lista:")
+# print(f'A sua lista contém, {item1}, {item2}, {item3}, {item4}')
+
+# adj1 = input("Digite um adjetivo:")
+# adj2 = input("Digite outro adjetivo:")
+# adj3 = input("Digite novamente outro adjetivo:")
+# print(f'Joguei durante horas um jogo {adj1} que contem cenários {adj2} e personagens {adj3}')
