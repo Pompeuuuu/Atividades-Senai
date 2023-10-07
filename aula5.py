633c68b6613f75f83bdc97c402b3152549ecb2cd
Pedro Pompeu <141966855+Pompeuuuu@users.noreply.github.com>
2023-10-07 15:23:55 -0300

    Add files via upload

---
+++ aula5.py
@@ -0,0 +1,33 @@
+# for vinte in range(2,21,2):
+#     print(vinte)
+
+# num1 = range(5,51,5)
+# soma=sum(num1)
+# print(soma)
+
+# wishlist = 12
+# verifica=type(wishlist)
+# print(verifica)
+
+# name=input("Digite seu nome:")
+# cumprimento=(f'Olá seja bem vindo {name}!!')
+# print(cumprimento)
+
+# impar = range(1,11,2)
+# soma=sum(impar)
+# media=soma/len(impar)
+# print(media)
+
+# lista=['masan','panana','cerega','uba']
+# for i in lista:
+#     print(i,'isso nao é futa')
+
+# dado={
+# 'nome':'Julio',
+# 'sexo':'Masculino',
+# 'idade':'19',
+# 'profissao':'Programador'
+# }
+
+# for i1, i2 in dado.items():
+#     print(f'{i1} : {i2}')
