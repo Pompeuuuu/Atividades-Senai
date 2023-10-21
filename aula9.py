261949894833de851e9acfb6f02fefedeecc9ff9
Pedro Pompeu <141966855+Pompeuuuu@users.noreply.github.com>
2023-10-21 16:24:00 -0300

    Add files via upload

---
+++ aula9.py
@@ -0,0 +1,46 @@
+# print("Seja bem vindo ao Bar do Tião Faça suas escolhas!")
+# def pratos():
+#     print ('Pizza(1)', 'Hamburger(2)')
+#     escolha = input("Escolha seu prato:")
+#     if escolha == '1':
+#         pratos = 'Pizza'
+#     elif escolha == '2':
+#         pratos = 'Hamburger'
+#     else:
+#         print('Escolha invalida!')
+#     return pratos
+# def drinks():
+#     print ('Tequila(1)', 'Coca-Cola(2)')
+#     escolha = input("Escolha sua bebida:")
+#     if escolha == '1':
+#         drinks = 'Tequila'
+#     elif escolha == '2':
+#         drinks = 'Coca-Cola'
+#     else:
+#         print('Escolha invalida!')
+#     return drinks
+# pratos = pratos()
+# drinks = drinks()
+# print(f'Sua escolha de prato foi {pratos} e bebida {drinks}')
+
+
+# sinonimos_word={
+#     'mentiroso':'charlatão',
+#     'portugues':'lusitano',
+#     'brasileiro':'canarilho',
+#     'bonito' :'lindo',
+#     'babaca' :'idiota'
+# }
+# # palavra=input('Digite uma palavra:')
+# # if palavra in sinonimos_word:
+# #     print(sinonimos_word[palavra])
+# # else:
+# #     print(f'A palavra {palavra} não existe!')
+
+# print()
+
+# def function_com_condt(valor):
+#     if valor <11:
+#         return print("Valor negativo")
+#     else:
+#         return print("Valor positivo]")
