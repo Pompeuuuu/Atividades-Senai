2cb932252161cf3538595ec89f17b1fd5f9aed11
Pedro Pompeu <141966855+Pompeuuuu@users.noreply.github.com>
2023-10-28 14:51:34 -0300

    Add files via upload

---
+++ Ex12.py
@@ -0,0 +1,25 @@
+# #4
+# frutas = {'maçã', 'banana', 'laranja', 'pêra',' abacaxi'}
+# tamanho=len(frutas)
+# print(tamanho)
+
+#5
+# conjunto1= {1,2,3,4,5}
+# conjunto2= {6,7,8,9,10}
+# print(conjunto1.union(conjunto2))
+
+#6
+# cores={'vermelho','verde','marrom','azul'}
+# cores.remove('vermelho')
+# print(cores)
+
+# 7
+# numeros=[1,2,3,4,5,6,7,8,9,10]
+# eh_par = list(filter(lambda x: x % 2 == 0,numeros))
+# print(eh_par)
+
+#8
+# alunos_aprovados = {'Alica','Igor','Jane','John','Aman','Xander'}
+# alunos_todos = {'Alica','Igor','Jane','John','Aman','Xander','Jose','Pedro','Jota'}
+# sit= alunos_todos.difference(alunos_aprovados)
+# print(f'Os alunos reprovados foram {sit}')
