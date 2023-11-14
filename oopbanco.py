6d5a3016b18ffdb8367a6fdc9a21ba65b2d4c0bc
Pedro Pompeu <141966855+Pompeuuuu@users.noreply.github.com>
2023-11-14 16:11:31 -0300

    Update oopbanco.py

--- oopbanco.py
+++ oopbanco.py
@@ -1,29 +1,48 @@
 import random
-valor = random.randint(1,1000)
-print ('Bem vindo ao Banco sua conta báncaria tem: R$', valor,'!')
-opcao = input('Escolha o método:\n [1] Deposito \n [2] Saque\n')
-class Banco:
-    def __init__(self,valor):
-        self.valor = valor
-    def deposito(self):
-        deposito = int(input('Digite um valor para depositar'))
-        valor += deposito
-        return valor
-    
-    def saque(self):
-        saque = int(input('Digite um valor para sacar'))
-        if valor > saque:
-            valor -= saque 
-            return valor
-        else:
-            print('Valor insuficiente!') 
-if opcao == '1':
-    banco = Banco(valor)
-    banco.deposito()
-    print(f'Sua conta possui agora {valor}')
-elif opcao == '2':
-    banco = Banco(valor)
-    banco.saque()
-    print(f'Sua conta possui agora {valor}')
-else:
-    print('Opção Inválida!')
+continuar = True
+while continuar:
+    valor = random.randint(1,1000)
+    print(f'\nBem vindo ao Banco sua conta báncaria tem: R${valor}!')
+    opcao = input('Escolha o método:\n [1] Deposito \n [2] Saque\n')
+   
+    class Banco:
+        def __init__(self,valor):
+            self.valor = valor
+        def deposito(self):
+            deposito = int(input('Digite um valor para depositar: '))
+            print(f'Sua conta possui agora R${valor+deposito}!')
+        
+        def saque(self):
+            saque = int(input('Digite um valor para sacar: '))
+            if self.valor > saque:
+                print(f'Sua conta possui agora R${valor-saque}')
+            else:
+                print('Valor insuficiente!') 
+        def error(self):
+            print('Opção Inválida!')
+
+    if opcao == '1':
+        banco = Banco(valor)
+        banco.deposito()
+    elif opcao == '2':
+        banco = Banco(valor)
+        banco.saque()
+    else:
+        banco = Banco(valor)
+        banco.error()
+        if opcao == '1':
+            banco = Banco(valor)
+            banco.deposito()
+        elif opcao == '2':
+            banco = Banco(valor)
+            banco.saque()
+
+    continuar = str(input('Deseja continuar? [S/N]: ')).upper().strip()[0]
+    if continuar == 'N':
+        continuar = False
+        print('\nObrigado por usar nossos serviços!')
+    elif continuar == 'S':
+        continuar = True
+    else:
+        banco = Banco(valor)
+        banco.error()
